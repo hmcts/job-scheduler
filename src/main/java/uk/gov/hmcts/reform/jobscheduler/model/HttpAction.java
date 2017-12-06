59a8f04592dc26b0744f20b6e35db6f2346bbf8b
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-12-06 12:18:07 +0000

    Store action payload as String

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/model/HttpAction.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/model/HttpAction.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.jobscheduler.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.HttpMethod;
 
 import java.util.Map;
@@ -11,13 +10,13 @@ public class HttpAction {
     public final String url;
     public final HttpMethod method;
     public final Map<String, String> headers;
-    public final JsonNode body;
+    public final String body;
 
     public HttpAction(
         @JsonProperty("url") String url,
         @JsonProperty("method") HttpMethod method,
         @JsonProperty("headers") Map<String, String> headers,
-        @JsonProperty("body") JsonNode body
+        @JsonProperty("body") String body
     ) {
         this.url = url;
         this.method = method;
