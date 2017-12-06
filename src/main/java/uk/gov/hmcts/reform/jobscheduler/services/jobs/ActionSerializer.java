5d2840fa4c13bafa1c3cbbfccdc86a3968a9fac6
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-12-06 12:16:09 +0000

    Add method for action deserialization

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/ActionSerializer.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/ActionSerializer.java
@@ -6,17 +6,27 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.jobscheduler.model.HttpAction;
 import uk.gov.hmcts.reform.jobscheduler.services.jobs.exceptions.JobActionSerializationException;
 
+import java.io.IOException;
+
+// Quartz can store only basic types as job data, we serialize it to json string.
 @Component
 public class ActionSerializer {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Quartz can store only basic types as job data, we serialize it to json string.
     public String serialize(HttpAction action) {
         try {
             return objectMapper.writeValueAsString(action);
         } catch (JsonProcessingException exc) {
             throw new JobActionSerializationException("Unable to serialize action", exc);
+        }
+    }
+
+    public HttpAction deserialize(String jsonString) {
+        try {
+            return objectMapper.readValue(jsonString, HttpAction.class);
+        } catch (IOException exc) {
+            throw new JobActionSerializationException("Unable to deserialize string to action", exc);
         }
     }
 }
