9a49a5ae57e356d2e9c36bbe9666253f64aa4807
leszekg <gonczar@gmail.com>
2017-12-12 16:37:29 +0000

    Make Quartz use PostgreSQL database as the job store

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/config/QuartzConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/config/QuartzConfiguration.java
@@ -3,19 +3,33 @@ package uk.gov.hmcts.reform.jobscheduler.config;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import javax.inject.Singleton;
 
 @Configuration
+@ConfigurationProperties
 public class QuartzConfiguration {
+
+    private final Map<String, String> quartzProperties = new HashMap<>();
+
+    // this getter is needed by the framework
+    public Map<String, String> getQuartzProperties() {
+        return quartzProperties;
+    }
 
     @Bean
     @Singleton
     public Scheduler scheduler() throws SchedulerException {
-        // todo: use database as job store
-        StdSchedulerFactory factory = new StdSchedulerFactory();
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties);
+
+        StdSchedulerFactory factory = new StdSchedulerFactory(properties);
         Scheduler scheduler = factory.getScheduler();
 
         scheduler.start();
