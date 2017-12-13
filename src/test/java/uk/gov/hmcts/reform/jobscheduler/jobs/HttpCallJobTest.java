cb7df44d5cfb642f3d487d7d3213d77de7ed4bf3
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-12-13 15:56:42 +0000

    Update test

--- src/test/java/uk/gov/hmcts/reform/jobscheduler/jobs/HttpCallJobTest.java
+++ src/test/java/uk/gov/hmcts/reform/jobscheduler/jobs/HttpCallJobTest.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.jobscheduler.jobs;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.google.common.collect.ImmutableMap;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -15,11 +15,14 @@ import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.jobscheduler.config.ApplicationConfiguration;
 import uk.gov.hmcts.reform.jobscheduler.model.HttpAction;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-
-import static java.util.Collections.emptyMap;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
@@ -29,32 +32,35 @@ public class HttpCallJobTest {
     public WireMockRule wireMockRule = new WireMockRule();
 
     @Autowired RestTemplate restTemplate;
+
     @Mock private ActionExtractor actionExtractor;
+    @Mock private JobExecutionContext context;
 
     @Test
-    public void execute_should_send_http_request_to_url_saved_in_job_details() {
+    public void execute_should_send_http_request_specified_in_job_details() {
 
-        JobExecutionContext context = mock(JobExecutionContext.class);
-
-        HttpAction actionToPerform =
-            new HttpAction(
-                "http://localhost:8080/hello-world",
-                HttpMethod.POST,
-                emptyMap(),
-                null
-            );
-
+        // given
         stubFor(
             post(urlEqualTo("/hello-world"))
                 .willReturn(aResponse().withStatus(200))
         );
 
-        given(actionExtractor.extract(context)).willReturn(actionToPerform);
+        given(actionExtractor.extract(context))
+            .willReturn(new HttpAction(
+                "http://localhost:8080/hello-world",
+                HttpMethod.POST,
+                ImmutableMap.of("ServiceAuthorization", "some-token"),
+                "some-body"
+            ));
 
         // when
-        HttpCallJob job = new HttpCallJob(restTemplate, actionExtractor);
-        job.execute(context);
+        new HttpCallJob(restTemplate, actionExtractor).execute(context);
 
         // then
+        verify(
+            postRequestedFor(urlEqualTo("/hello-world"))
+                .withHeader("ServiceAuthorization", equalTo("some-token"))
+                .withRequestBody(equalTo("some-body"))
+        );
     }
 }
