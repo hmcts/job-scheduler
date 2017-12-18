49e5a0c199a47395c8a0469fc82f2807e032af9b
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2017-12-18 09:46:40 +0000

    Remove totp_ prefix from variables
    Seems unnecessary

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/config/ServiceTokenGeneratorConfiguration.java
@@ -18,7 +18,7 @@ public class ServiceTokenGeneratorConfiguration {
 
     @Bean
     public AuthTokenGenerator serviceAuthTokenGenerator(
-        @Value("${idam.s2s-auth.totp_secret}") final String secret,
+        @Value("${idam.s2s-auth.secret}") final String secret,
         @Value("${idam.s2s-auth.microservice}") final String microService,
         final ServiceAuthorisationApi serviceAuthorisationApi
     ) {
