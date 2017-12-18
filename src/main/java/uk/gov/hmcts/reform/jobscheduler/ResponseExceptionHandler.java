ee7b59bff2a0bbba31599a36af2261375315079c
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-12-18 11:39:06 +0000

    Cleanup exception handler

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/ResponseExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/ResponseExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import uk.gov.hmcts.reform.jobscheduler.services.auth.AuthException;
 import uk.gov.hmcts.reform.jobscheduler.services.jobs.exceptions.JobNotFoundException;
@@ -33,23 +32,26 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(NotImplementedException.class)
-    protected ResponseEntity handleNotImplemented(NotImplementedException exc, WebRequest req) {
-        return status(HttpStatus.NOT_IMPLEMENTED).build();
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    protected void handleNotImplemented() {
+        // empty method
     }
 
     @ExceptionHandler(FeignException.class)
-    protected ResponseEntity handleFeignException(FeignException exc, WebRequest req) {
+    protected ResponseEntity<Object> handleFeignException(FeignException exc) {
         return status(exc.status()).build();
     }
 
     @ExceptionHandler(AuthException.class)
-    protected ResponseEntity handleAuthException(AuthException exc, WebRequest req) {
-        return status(HttpStatus.UNAUTHORIZED).build();
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    protected void handleAuthException() {
+        // empty method
     }
 
     @ExceptionHandler(JobNotFoundException.class)
-    protected ResponseEntity handleNotFoundException(AuthException exc, WebRequest req) {
-        return status(HttpStatus.NOT_FOUND).build();
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected void handleNotFoundException() {
+        // empty method
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
