d11a457aa7bc7acf868d96bdac1b7d96f7b1fdf0
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-12-20 15:23:05 +0000

    Don't trace ServiceAuthorization header

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/config/ApplicationConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/config/ApplicationConfiguration.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.jobscheduler.config;
 
+import org.springframework.boot.actuate.trace.TraceProperties;
+import org.springframework.boot.actuate.trace.TraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.api.filters.SensitiveHeadersRequestTraceFilter;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -10,5 +13,13 @@ public class ApplicationConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public SensitiveHeadersRequestTraceFilter requestTraceFilter(
+        TraceRepository traceRepository,
+        TraceProperties traceProperties
+    ) {
+        return new SensitiveHeadersRequestTraceFilter(traceRepository, traceProperties);
     }
 }
