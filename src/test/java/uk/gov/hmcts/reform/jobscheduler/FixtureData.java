886bd84a8bf8a85dc82e9f40de1e96ee4bf372f7
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-12-22 09:19:25 +0000

    Remove `spring-cloud-starter-openfeign` dependency

--- src/test/java/uk/gov/hmcts/reform/jobscheduler/FixtureData.java
+++ src/test/java/uk/gov/hmcts/reform/jobscheduler/FixtureData.java
@@ -13,11 +13,12 @@ public final class FixtureData {
     }
 
     public static AbstractAuthorisationException getAuthorisationException() {
-        feign.Response feignResponse = feign.Response
-            .builder()
-            .status(HttpStatus.UNAUTHORIZED.value())
-            .headers(Collections.emptyMap())
-            .build();
+        feign.Response feignResponse = feign.Response.create(
+            HttpStatus.UNAUTHORIZED.value(),
+            "i must fail",
+            Collections.emptyMap(),
+            new byte[0]
+        );
         FeignException feignException = FeignException.errorStatus("oh no", feignResponse);
 
         return AbstractAuthorisationException.parseFeignException(feignException);
