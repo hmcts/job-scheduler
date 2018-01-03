c373446b8f4492630ea4611b78e962cd43c67590
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-01-03 13:03:27 +0000

    Remove unused method

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/GetterFromScheduler.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/GetterFromScheduler.java
@@ -13,15 +13,11 @@ public class GetterFromScheduler {
     private GetterFromScheduler() {
     }
 
-    static <T> T call(ThrowingSupplier<T> supplier, String message) {
-        try {
-            return supplier.get();
-        } catch (SchedulerException exc) {
-            throw new JobException(message, exc);
-        }
-    }
-
     public static <T> T call(ThrowingSupplier<T> throwingSupplier) {
-        return call(throwingSupplier, "Error while getting information from scheduler");
+        try {
+            return throwingSupplier.get();
+        } catch (SchedulerException exc) {
+            throw new JobException(exc.getMessage(), exc);
+        }
     }
 }
