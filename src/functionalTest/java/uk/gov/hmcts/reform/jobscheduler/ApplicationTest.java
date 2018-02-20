2de82647992687661ee3d20d651715b6758152a0
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-02-20 20:30:13 +0000

    Add functional test to check context loads

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/jobscheduler/ApplicationTest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.jobscheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        // context load
+    }
+
+}
