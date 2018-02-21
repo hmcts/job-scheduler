00af48aa33e371fa9f700e92f3d93818b3f36cc0
Jason Paige <jasonpaige@users.noreply.github.com>
2018-02-21 13:04:29 +0000

    Fixes SonarQube issues (#65)
    * Fix wiremock port conflict on jenkins
    
    * Fixes sonar issues

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/model/JobSchedulerPageRequest.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/model/JobSchedulerPageRequest.java
@@ -5,14 +5,14 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import org.springframework.data.domain.Sort;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public final class PageRequest extends org.springframework.data.domain.PageRequest {
+public final class JobSchedulerPageRequest extends org.springframework.data.domain.PageRequest {
 
     @Deprecated
-    private PageRequest(int page, int size, Sort sort) {
+    private JobSchedulerPageRequest(int page, int size, Sort sort) {
         super(page, size, sort);
     }
 
-    public static PageRequest of(int page, int size) {
-        return new PageRequest(page, size, null);
+    public static JobSchedulerPageRequest of(int page, int size) {
+        return new JobSchedulerPageRequest(page, size, null);
     }
 }
