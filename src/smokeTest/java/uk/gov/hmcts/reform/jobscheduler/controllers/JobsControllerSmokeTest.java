08aea3b74e5db05b3a296ca1c43a410a0bf035aa
Jason Paige <jason.r.paige@gmail.com>
2018-02-22 10:33:58 +0000

    Revert to non-spring solution for smoke tests

--- src/smokeTest/java/uk/gov/hmcts/reform/jobscheduler/controllers/JobsControllerSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/jobscheduler/controllers/JobsControllerSmokeTest.java
@@ -3,21 +3,27 @@ package uk.gov.hmcts.reform.jobscheduler.controllers;
 import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.http.HttpHeaders;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
 public class JobsControllerSmokeTest {
 
-    @Value("${default-test-url:http://localhost:8484}")
-    private String testUrl;
-
     @Before
-    public void setup() {
-        System.out.println(testUrl);
-        RestAssured.baseURI = testUrl;
+    public void setup() throws IOException {
+        Map<String, String> env = System.getenv();
+        if (env.get("TEST_URL") == null) {
+            Resource resource = new ClassPathResource("/smoke-application.properties");
+            Properties props = PropertiesLoaderUtils.loadProperties(resource);
+            RestAssured.baseURI = props.getProperty("default-test-url");
+        } else {
+            RestAssured.baseURI = env.get("TEST_URL");
+        }
     }
 
     @Test
