957f50b3314f87719ab11d61cac2ba8cd0c14e4f
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-02-26 17:36:32 +0000

    Improve test context

--- src/functionalTest/java/uk/gov/hmcts/reform/jobscheduler/controllers/JobsControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/jobscheduler/controllers/JobsControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -26,8 +27,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @AutoConfigureMockMvc
+@ContextConfiguration
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JobsControllerTest {
