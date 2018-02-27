a467fb97e8b18cd44d10618f4511ac1422e98f4d
donatas M <doncem@users.noreply.github.com>
2018-02-27 08:01:15 +0000

    Improve functional tests (#75)
    * Improve test context
    
    * Speed up boot by disabling testcontainers' checks

--- src/functionalTest/java/uk/gov/hmcts/reform/jobscheduler/ApplicationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/jobscheduler/ApplicationTest.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.reform.jobscheduler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ContextConfiguration
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
