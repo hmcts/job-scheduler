545f4b0bc374dd9497ef88ea374f285101d65ebd
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-13 12:57:29 +0000

    Implement AbstractLoggingException so log events are correct

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/exceptions/JobNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/exceptions/JobNotFoundException.java
@@ -1,4 +1,13 @@
 package uk.gov.hmcts.reform.jobscheduler.services.jobs.exceptions;
 
-public class JobNotFoundException extends RuntimeException {
+import uk.gov.hmcts.reform.logging.exception.AbstractLoggingException;
+import uk.gov.hmcts.reform.logging.exception.AlertLevel;
+
+import static uk.gov.hmcts.reform.jobscheduler.logging.ErrorCode.UNKNOWN;
+
+public class JobNotFoundException extends AbstractLoggingException {
+
+    public JobNotFoundException() {
+        super(AlertLevel.P4, UNKNOWN, (Throwable) null);
+    }
 }
