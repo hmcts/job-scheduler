680ceeb86804181c4a7c8d829b123e8c06aacd18
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-21 14:33:04 +0000

    Update the welcome message

--- src/test/java/uk/gov/hmcts/reform/jobscheduler/controllers/GetWelcomeTest.java
+++ src/test/java/uk/gov/hmcts/reform/jobscheduler/controllers/GetWelcomeTest.java
@@ -7,12 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 import uk.gov.hmcts.reform.jobscheduler.services.jobs.JobsService;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -30,8 +29,8 @@ public class GetWelcomeTest {
 
     @Test
     public void should_welcome_upon_root_request_with_200_response_code() throws Exception {
-        MvcResult response = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
-
-        assertThat(response.getResponse().getContentAsString()).startsWith("Welcome");
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Welcome to Job Scheduler"));
     }
 }
