ecfbffe4f86aa1714fb7c2c04f1f5ab7e07e051e
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-21 13:15:01 +0000

    Introduce RootController

---
+++ src/test/java/uk/gov/hmcts/reform/jobscheduler/controllers/GetWelcomeTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.jobscheduler.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+import uk.gov.hmcts.reform.jobscheduler.services.jobs.JobsService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class GetWelcomeTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JobsService service; // NOPMD we only need context to load
+
+    @MockBean
+    private AuthTokenValidator tokenValidator; // NOPMD we only need context to load
+
+    @Test
+    public void should_welcome_upon_root_request_with_200_response_code() throws Exception {
+        MvcResult response = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
+
+        assertThat(response.getResponse().getContentAsString()).startsWith("Welcome");
+    }
+}
