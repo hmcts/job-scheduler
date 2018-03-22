5044beda7f19906395658dd62e28e52bcc3e9e48
donatas M <doncem@users.noreply.github.com>
2018-03-22 11:32:13 +0000

    Fix SonarQube error on inheritance (#92)

--- src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/exceptions/JobActionSerializationException.java
+++ src/main/java/uk/gov/hmcts/reform/jobscheduler/services/jobs/exceptions/JobActionSerializationException.java
@@ -3,6 +3,10 @@ package uk.gov.hmcts.reform.jobscheduler.services.jobs.exceptions;
 import uk.gov.hmcts.reform.logging.exception.AlertLevel;
 import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
 
+/**
+ * SonarQube reports as error. Max allowed - 5 parents
+ */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
 public class JobActionSerializationException extends UnknownErrorCodeException {
 
     public JobActionSerializationException(String message, Throwable cause) {
